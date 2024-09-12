70fecbb16ce9c7c3f90213c3f3a998a84ba13365
pedru44 <pedroc@iesb.edu.br>
2024-09-12 00:25:25 -0300

    calculadora pronta

--- Calculadora.js
+++ Calculadora.js
@@ -19,6 +19,49 @@ class Calculadora {
         this.#resultado = numero;
     }
 
+    soma(valor) {
+        if (typeof valor === 'string') {
+            valor = parseFloat(valor); 
+        }
+        if (isNaN(valor) || typeof valor !== 'number') {
+            throw new TypeError("O argumento deve ser um número válido");
+        }
+        this.#resultado += valor; 
+    }
+    subtrai(valor) {
+        if (typeof valor === 'string') {
+            valor = parseFloat(valor); 
+        }
+        if (isNaN(valor) || typeof valor !== 'number') {
+            throw new TypeError("O argumento deve ser um número válido");
+        }
+        this.#resultado -= valor; 
+    }
+    multiplica(valor) {
+        if (typeof valor === 'string') {
+            valor = parseFloat(valor); 
+        }
+        if (isNaN(valor) || typeof valor !== 'number') {
+            throw new TypeError("O argumento deve ser um número válido");
+        }
+        this.#resultado *= valor; 
+    }
+    divide(valor) {
+        if (typeof valor === 'string') {
+            valor = parseFloat(valor); 
+        }
+        if (isNaN(valor) || typeof valor !== 'number') {
+            throw new TypeError("O argumento deve ser um número válido");
+        }
+        if (valor === 0) {
+            throw new Error("divisão ilegal por zero");
+        }
+        this.#resultado /= valor;
+    }
+    inverte() {
+        this.#resultado = -this.#resultado; 
+    }
+
 }
 
 module.exports = Calculadora;
--- Calculadora.test.js
+++ Calculadora.test.js
@@ -10,7 +10,6 @@ describe("Calculadora", () => {
     });
     it("Obtém resultado", () => {
         const calculadora = new Calculadora();
-        //expect(calculadora).toBeDefined();
         expect(calculadora.resultado).toBeDefined();
         expect(typeof calculadora.resultado).toBe("number");
     });
@@ -23,4 +22,68 @@ describe("Calculadora", () => {
         expect(() => calculadora.resultado = "5").not.toThrow(TypeError);
         expect(calculadora.resultado).toBe(5);
     });
+    it("Soma dos valores numéricos", () => {
+        const calculadora = new Calculadora();
+        expect(() => calculadora.soma(10)).not.toThrow();
+        expect(calculadora.resultado).toBe(10);
+        expect(() => calculadora.soma("5")).not.toThrow();
+        expect(calculadora.resultado).toBe(15);
+        expect(() => calculadora.soma("Texto")).toThrow(TypeError);
+        expect(() => calculadora.soma("Texto"))
+             .toThrow("O argumento deve ser um número válido");
+        expect(calculadora.resultado).toBe(15);
+    });
+    it("Subtrai os valores numéricos", () => {
+        const calculadora = new Calculadora();
+        calculadora.resultado = 20;
+        expect(() => calculadora.subtrai(10)).not.toThrow();
+        expect(calculadora.resultado).toBe(10);
+        expect(() => calculadora.subtrai("5")).not.toThrow();
+        expect(calculadora.resultado).toBe(5);
+        expect(() => calculadora.subtrai("Texto")).toThrow(TypeError);
+        expect(() => calculadora.subtrai("Texto"))
+             .toThrow("O argumento deve ser um número válido");
+        expect(calculadora.resultado).toBe(5);
+    });
+    it("Multiplica os valores numéricos", () => {
+        const calculadora = new Calculadora();
+        calculadora.resultado = 10;
+        expect(() => calculadora.multiplica(2)).not.toThrow();
+        expect(calculadora.resultado).toBe(20);
+        expect(() => calculadora.multiplica("3")).not.toThrow();
+        expect(calculadora.resultado).toBe(60);
+        expect(() => calculadora.multiplica("Texto")).toThrow(TypeError);
+        expect(() => calculadora.multiplica("Texto"))
+            .toThrow("O argumento deve ser um número válido");
+        expect(calculadora.resultado).toBe(60);
+    });
+    it("Divide os valores numéricos", () => {
+        const calculadora = new Calculadora();
+        calculadora.resultado = 20;
+        expect(() => calculadora.divide(2)).not.toThrow();
+        expect(calculadora.resultado).toBe(10);
+        expect(() => calculadora.divide("5")).not.toThrow();
+        expect(calculadora.resultado).toBe(2);
+        expect(() => calculadora.divide(0)).toThrow(Error);
+        expect(() => calculadora.divide(0)).toThrow("divisão ilegal por zero");
+        expect(() => calculadora.divide("0")).toThrow(Error);
+        expect(() => calculadora.divide("0")).toThrow("divisão ilegal por zero");
+        expect(calculadora.resultado).toBe(2);
+    });
+    it("Inverte o sinal do resultado", () => {
+        const calculadora = new Calculadora();
+        calculadora.resultado = 10;
+        expect(() => calculadora.inverte()).not.toThrow();
+        expect(calculadora.resultado).toBe(-10);
+        expect(() => calculadora.inverte()).not.toThrow();
+        expect(calculadora.resultado).toBe(10);
+        calculadora.resultado = 0;
+        expect(() => calculadora.inverte()).not.toThrow();
+        expect(calculadora.resultado).toBeCloseTo(0); 
+        calculadora.resultado = "5";
+        expect(() => calculadora.inverte()).not.toThrow();
+        expect(calculadora.resultado).toBe(-5);
+    });
+    
+    
 })
